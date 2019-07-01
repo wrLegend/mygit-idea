d554d2069e6aa6de76b6592a2247cc4ccfc378c3
wrLegend <wrangler1024@163.com>
2019-07-01 16:41:46 +0800

    写了一个User类

---
+++ mygit/src/main/java/com/itheima/User.java
@@ -0,0 +1,19 @@
+package com.itheima;
+
+/**
+ * @author Jeffery
+ * @version 1.0
+ * @description
+ * @date 2019/7/1 16:37
+ */
+public class User {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
